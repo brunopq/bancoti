38e04df9f9f286a5e5e166783c3494c4be8c7efd
Bruno Panizzi <bruno@ibotiadvogados.com.br>
2025-07-23 10:32:02 -0300

    changes to the build script

--- build.ts
+++ build.ts
@@ -1,17 +1,38 @@
 import { build, $ } from "bun"
 
+const prodDeps = ["node-cron", "pino", "pino-pretty", "pino-loki"]
+
 const start = Date.now()
 
 await build({
   entrypoints: ["./src/index.ts"],
   outdir: "./dist",
   format: "esm",
-  minify: true,
+  minify: false,
   sourcemap: "linked",
   target: "node",
+  external: prodDeps,
 })
 
 await $`cp -r ./drizzle ./dist/drizzle`
+
+const pkgJson = await Bun.file("./package.json").json()
+
+const prodPkgJson = {
+  name: pkgJson.name,
+  version: pkgJson.version,
+  main: "index.js",
+  type: "module",
+  dependencies: prodDeps.reduce(
+    (acc, dep) => {
+      acc[dep] = pkgJson.dependencies[dep]
+      return acc
+    },
+    {} as Record<string, string>,
+  ),
+}
+
+await Bun.write("./dist/package.json", JSON.stringify(prodPkgJson, null, 2))
 
 const end = Date.now()
 
